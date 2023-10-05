102a3c785d16a73a8957d1d8df94fd54d510ec42
reallypseudo <tarnekal@gmail.com>
2023-10-05 10:33:17 -0500

    perf fixes

--- ImgCli/ImgCli.cpp
+++ ImgCli/ImgCli.cpp
@@ -3,11 +3,15 @@
 #include <windows.h>
 #include <fstream>
 #define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
 #include "stb_image.h"
+#include "stb_image_resize.h"
 #include "colors.hpp"
 
 int main(int argc, char *argv[])
 {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
     if (argc < 2)
     {
         std::cout << "Missing 1 argument (file path)" << std::endl;
@@ -16,7 +20,8 @@ int main(int argc, char *argv[])
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     std::string density = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:, ^ `'.";
     int k,w,h,c,densityLen;
-    unsigned char* img = stbi_load(argv[1], &w, &h, &c, 3);
+    unsigned int bytePerPixel = 4;
+    unsigned char* img = stbi_load(argv[1], &w, &h, &c, bytePerPixel);
     SetConsoleTextAttribute(hConsole, 11);
 
     if (img == NULL)
@@ -48,14 +53,20 @@ int main(int argc, char *argv[])
     {
         for (int i = 0; i < w; i++)
         {
-            unsigned char* pixelOffset = img + (i + w * j) * (unsigned)c;
+            unsigned char* pixelOffset = img + (i + w * j) * bytePerPixel;
+            if (c > 3 && pixelOffset[3] < 20)
+            {
+                SetConsoleTextAttribute(hConsole, 0);
+                std::cout << " ";
+                continue;
+            }
             rgb color_rgb = {
                 (double)pixelOffset[0],
                 (double)pixelOffset[1],
                 (double)pixelOffset[2]
             };
             hsv color_hsv = rgb2hsv(color_rgb);
-            double indx = (((color_rgb.r+color_rgb.g+color_rgb.b)/3)/255)*densityLen;
+            double indx = (color_hsv.v/255)*densityLen;
             char ascii = density[(int)indx];
             SetConsoleTextAttribute(hConsole, consoleColor(color_hsv));
             std::cout << ascii;
